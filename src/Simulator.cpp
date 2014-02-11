2896d4bbcde8207cebba0896011f4e74a7122122
eschweic <eschweickart@mozilla.com>
2014-02-11 00:27:11 -0500

    Add data structures, introduce threading.

--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -7,7 +7,86 @@
 //
 
 #include "Simulator.h"
+#include <boost/thread/thread.hpp>
 
+// Implementations for Segment
+Segment::Segment(const CtrlPoint& a, const CtrlPoint& b) : first(a), second(b) {}
+const Vec3f inline Segment::vec() const { return second.pos - first.pos; }
+
+
+// Implementations for Yarn
+const size_t inline Yarn::numCPs() const { return points.size(); }
+const size_t inline Yarn::numIntCPs() const { return fmax(points.size()-2, 0); }
+const size_t inline Yarn::numSegs() const { return segments.size(); }
+
+
+// Implementations for Simulator
 void Simulator::run() {
-  std::cout << "hi!\n";
+  std::cout << "Starting simulation... \n";
+  
+  // TODO: import initial model/constraints
+  
+  
+  
+  /*
+   * precompute omegaBar^i_j ((2) of Bergou)
+   * set quasistatic material frame
+   * while simulating do
+   *   compute forces on centerline
+   *   integrate centerline
+   *   satisfy glue constraints
+   *   enforce inextensibility
+   *   collision detection/response
+   *   update Bishop frame (parallel transport through time)
+   *   update quasistatic material frame
+   */
+  
+  bool done = false;
+
+  // MAIN SIMULATION LOOP
+  while (!done) {
+    computeForces();
+    
+    done = true;
+  }
+  
+  std::cout << "Simulation completed successfully!\n";
+  std::exit(0);
+}
+
+
+
+void Simulator::computeForces() {
+  /*
+   * calculate bending energy
+   * calculate twisting energy
+   *
+   */
+  
+  // Rename some variables to allow capture
+  const Yarn& yarnRef = y;
+  std::vector<Vec3f>& cbsRef = ws.curvatureBinormals;
+  auto f = [&yarnRef, &cbsRef](size_t offset, size_t size) -> void {
+    for (size_t i=offset; i<size; i++) {
+      Vec3f ePrev = yarnRef.segments[i].vec();
+      Vec3f eNext = yarnRef.segments[i+1].vec();
+      cbsRef[i] = 2*ePrev.cross(eNext) /
+                  (yarnRef.segments[i].restLength*yarnRef.segments[i+1].restLength +
+                   ePrev.dot(eNext));
+    }
+  };
+  
+  if (y.numIntCPs() > 1000) { // Are threads really helpful here?
+    for (int i=0; i<NUM_THREADS; i++) {
+      ws.threads[i] = boost::thread(f,
+                                    (i*y.numIntCPs())/NUM_THREADS,
+                                    ((i+1)*y.numIntCPs())/NUM_THREADS);
+    }
+    for (int i=0; i<NUM_THREADS; i++) {
+      ws.threads[i].join();
+    }
+  } else { // Just do the work on the main thread
+    f(0, y.numIntCPs());
+  }
+  
 }
--- src/Simulator.h
+++ src/Simulator.h
@@ -10,10 +10,84 @@
 #define __Visualizer__Simulator__
 
 #include <iostream>
+#include "Eigen/Dense"
+#include <boost/thread/thread.hpp>
+
+#define NUM_THREADS 4
+
+typedef Eigen::Vector3f Vec3f;
+
+struct CtrlPoint
+{
+  /// Position of the control point in space.
+  Vec3f pos;
+  /// The accumulated force at this control point.
+  Vec3f force;
+};
+
+
+// TODO: move to separate class?
+class Segment
+{
+  /// Reference to the first control point that defines this segment.
+  const CtrlPoint& first;
+  /// Reference to the second control point that defines this segment.
+  const CtrlPoint& second;
+  /// The u vector of the twist-free reference (Bishop) frame.
+  Vec3f u;
+  /// The v vector of the twist-free reference (Bishop) frame.
+  Vec3f v;
+  /// Rotation in radians of the material frame.
+  float rot = 0;
+
+public:
+  /// Default Segment constructor
+  Segment(const CtrlPoint&, const CtrlPoint&);
+  /// Get the vector that represents this segment. Not necessarily unit length.
+  const Vec3f inline vec() const;
+  /// Length of the segment at rest.
+  float restLength;
+  
+  // TODO: Need Catmull-Rom interpolation for contact resolution.
+};
+
+
+// TODO: move to separate class?
+class Yarn
+{
+public:
+  /// Control points (n+1 total) that define the yarn's position.
+  std::vector<CtrlPoint> points;
+  /// Segments (n total) that define the yarn.
+  std::vector<Segment> segments;
+  /// Get the number of control points on the yarn.
+  const size_t inline numCPs() const;
+  /// Get the number of control points associated with 2 edges.
+  const size_t inline numIntCPs() const;
+  /// Get the number of segments in the yarn.
+  const size_t inline numSegs() const;
+  
+  // TODO: enforce vector size invariants
+};
+
+struct Workspace
+{
+  /// "Thread pool" for parallel procedures. In quotes because, as far as I know, threads are
+  /// re-initialized each time boost::thread(f) is called.
+  boost::thread threads[NUM_THREADS];
+  /// Curvature binormals for the centerline. Notice this is undefined for the first and final
+  /// control points.
+  std::vector<Vec3f> curvatureBinormals;
+};
 
 class Simulator
 {
+  Yarn y;
+  Workspace ws;
+  
+  void computeForces();
 public:
+  /// Entry point for simulator; called after the app has finished initializing
   void run();
 };
 
